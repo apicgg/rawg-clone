96f7c6a04b8ffe2fbfcd9c3fca13c1e3bafbed47
Anurag Pramanik <p.anurag@outlook.com>
2023-03-24 21:34:27 +0530

    Add custom hook to fetch games

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,43 +1,8 @@
 import { Text } from "@chakra-ui/react";
-import { isAxiosError } from "axios";
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
-
-interface Game {
-  id: number;
-  name: string;
-}
-
-interface FetchGameResponse {
-  count: number;
-  results: Game[];
-}
+import useGames from "../hooks/useGames";
 
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    const fetchGames = async () => {
-      try {
-        const response = await apiClient.get<FetchGameResponse>("/games");
-        setGames(response.data.results);
-      } catch (error) {
-        if (isAxiosError(error)) {
-          setError(error.message);
-        }
-      }
-    };
-
-    fetchGames();
-  }, []);
-
-  // useEffect(() => {
-  //   apiClient
-  //     .get<FetchGameResponse>("/xgames")
-  //     .then((res) => setGames(res.data.results))
-  //     .catch((err) => setError(err.message));
-  // }, []);
+  const { games, error } = useGames();
 
   return (
     <>
---
+++ src/hooks/useGames.ts
@@ -0,0 +1,51 @@
+import { CanceledError, isAxiosError } from "axios";
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+
+interface Game {
+  id: number;
+  name: string;
+}
+
+interface FetchGameResponse {
+  count: number;
+  results: Game[];
+}
+
+const useGames = () => {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+
+  // useEffect(() => {
+  //   apiClient
+  //     .get<FetchGameResponse>("/xgames")
+  //     .then((res) => setGames(res.data.results))
+  //     .catch((err) => setError(err.message));
+  // }, []);
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    const fetchGames = async () => {
+      try {
+        const response = await apiClient.get<FetchGameResponse>("/games", {
+          signal: controller.signal,
+        });
+        setGames(response.data.results);
+      } catch (error) {
+        if (error instanceof CanceledError) return;
+        if (isAxiosError(error)) {
+          setError(error.message);
+        }
+      }
+    };
+
+    fetchGames();
+
+    return () => controller.abort();
+  }, []);
+
+  return { games, error };
+};
+
+export default useGames;
