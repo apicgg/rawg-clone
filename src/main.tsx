ce94f8db2f9684cf9bfce3074fa14f3aedb535aa
Anurag Pramanik <p.anurag@outlook.com>
2023-03-29 17:45:17 +0530

    Move theme.ts to correct dir

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import theme from "./theme";
+import theme from "./components/Theme/theme";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
