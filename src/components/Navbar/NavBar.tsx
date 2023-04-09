edb1e6e2d226489e2857d8d59a3a7a78b9185569
Anurag Pramanik <p.anurag@outlook.com>
2023-04-09 20:54:53 +0530

    Implement search

--- src/components/Navbar/NavBar.tsx
+++ src/components/Navbar/NavBar.tsx
@@ -1,13 +1,16 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Image } from "@chakra-ui/react";
 import logo from "../../assets/Logo/logo.webp";
 import SearchInput from "../Search/SearchInput";
 import ColorModeSwitch from "../Theme/ColorModeSwitch";
+interface Props {
+  onSearch: (searchText: string) => void;
+}
 
-const NavBar = () => {
+const NavBar = ({ onSearch }: Props) => {
   return (
     <HStack padding="10px">
       <Image src={logo} boxSize="60px" />
-      <SearchInput />
+      <SearchInput onSearch={onSearch} />
       <ColorModeSwitch />
     </HStack>
   );
--- src/components/Search/SearchInput.tsx
+++ src/components/Search/SearchInput.tsx
@@ -1,14 +1,33 @@
 import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import React, { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
 
-const SearchInput = () => {
+interface Props {
+  onSearch: (searchText: string) => void;
+}
+
+const SearchInput = ({ onSearch }: Props) => {
+  const inpurRef = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    if (inpurRef.current) onSearch(inpurRef.current.value);
+  };
+
   return (
-    <InputGroup>
-      <InputLeftElement>
-        <BsSearch />
-      </InputLeftElement>
-      <Input borderRadius={20} placeholder="Search games..." variant="filled" />
-    </InputGroup>
+    <form onSubmit={handleSubmit}>
+      <InputGroup>
+        <InputLeftElement>
+          <BsSearch />
+        </InputLeftElement>
+        <Input
+          ref={inpurRef}
+          borderRadius={20}
+          placeholder="Search games..."
+          variant="filled"
+        />
+      </InputGroup>
+    </form>
   );
 };
 
